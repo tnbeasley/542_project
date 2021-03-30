14b84dafd8a5f0396c4e9d4a64a722d4fa3e7122
tnbeasley <clp292@vols.utk.edu>
2021-03-29 21:25:16 -0400

    Added basic framework to kick-start data modeling pipeline

--- bankruptcy.py
+++ bankruptcy.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import numpy as np
 
+def import_data():
+    return df
+
+def clean_data(df):
+    return clean_df
+
+def prepare_model_data(df):
+    return model_df
+
+if __name__ == 'main':
+    df = import_data()
+    clean_df = clean_data(df)
+    model_df = prepare_model_data(clean_df)
