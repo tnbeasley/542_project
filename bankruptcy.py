b5a7b03bd75682422d7ee415c6c8a07b54486845
Yang Li <fivecentsly@gmail.com>
2021-04-02 23:33:25 -0400

    Add data clean code

--- bankruptcy.py
+++ bankruptcy.py
@@ -17,16 +17,35 @@ def clean_data(df):
     """
     
     # Fix column names
+    # Create a dict to store original column names and new column names x1, x2...
+    ncol = df.shape[1]
+    keys = df.columns[1:] # original column names
+    values = [''.join(['x', str(x)]) for x in range(1, ncol)] # new column names
+    col_index_dict = dict(zip(keys, values))
     
-    # Drop columns with few/one value
+    # Export column name index as a dataframe
+    col_index_df = pd.DataFrame(index=values)
+    col_index_df['col_name'] = keys
+    col_index_df.to_csv('col_index.csv')
+    
+    # Rename column names
+    df.rename(columns=col_index_dict, inplace=True)
+    
+    # Drop columns with only one value (not contribute to prediction model)
+    # Check which columns have only one value - return indices and original column names
+    col_ind = [i for i in range(0, ncol) if len(df.iloc[:,i].value_counts()) <= 1]
+    col_names = [df.columns[i] for i in col_ind]
+    # Drop the columns
+    print(f'Dropping columns {col_names} that have only one value')
+    df.drop(col_names, axis=1, inplace=True)
     
     # Drop NA values (if needed)
-    num_na_rows = (df.apply(lambda x: sum(x.isnull().values), axis = 1) > 0).sum()
+    # Drop NA values (if needed)
+    num_na_rows = (df.apply(lambda x: sum(x.isnull().values), axis=1) > 0).sum()
     print(f'Dropping {num_na_rows} row(s) with NAs')
-    df1 = df.dropna()
+    df.dropna(inplace=True)
     
-    # return clean_df
-    pass
+    return df
 
 
 # df = import_data()
